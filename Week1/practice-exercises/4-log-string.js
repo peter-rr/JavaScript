c93cfddc9e65094036f03efc40fde36a2f651ea6
Pedro Ramos <pedroramosromero@gmail.com>
2021-08-25 14:08:52 +0200

    Commit all deleted and modified files.

--- Week1/practice-exercises/4-log-string.js
+++ Week1/practice-exercises/4-log-string.js
@@ -4,20 +4,20 @@
  */
 
 // 1. Declare a variable myString and assign a string to it. Use your full name, including spaces, as the content for the string.
-
+var myString = "Peter Funk";
 
 // 2. Write a console.log statement in which you explain in words what you think the value of the string is.
-
+console.log("I think the value of the string is the full name I've just assigned to it");
 
 // 3. Now console.log the variable myString.
-
+console.log(myString);
 
 // 4. Now reassign to the variable myString a new string.
-
+myString = "Tony Allen & Jeff Mills"
 
 // 5. Just like what you did before write a console.log statement that explains in words what you think will be logged to the console.
-
+console.log("I think the value of the string is the new full name I've reassigned to it");
 
 // 6. Now console.log myString again.
-
+console.log(myString);
 
--- Week1/practice-exercises/6-log-animals.js
+++ Week1/practice-exercises/6-log-animals.js
@@ -3,23 +3,23 @@
  */
 
 // 1. Declare variable and assign to it an empty array. Make sure that the name of the variable indicates it contains more than 1 item. For example items instead of item.
-
+var myThings = [];
 
 // 2. Write a console.log statement that explains in words what you think the value of the array is.
-
+console.log("I think the value of the array will be an empty array as it has been initialized like that");
 
 // 3. Write a console.log statement that logs the array.
-
+console.log(myThings);
 
 // 4. Create a new variable with an array that has 3 of your favorite animals, each in a different string. Make sure the name of the variables says something about what the variable contains.
-
+var myFavouriteAnimals = ['cheetah', 'dolphin', 'hawk'];
 
 // 5. Write a console.log statement that logs the second array.
-
+console.log(myFavouriteAnimals);
 
 // 6. Add a statement that adds another string ("Piglet)" to the array of animals.
+myFavouriteAnimals.push('piglet');
 
 
 // 7. Write a console.log statement that logs the second array!
-
-
+console.log(myFavouriteAnimals);
